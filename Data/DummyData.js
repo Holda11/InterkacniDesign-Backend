ad94f22db207decf50c7c8da0da04f38793bab23
Jan Holda <janholda@seznam.cz>
2024-11-14 14:37:52 +0100

    DummyData Added

---
+++ Data/DummyData.js
@@ -0,0 +1,95 @@
+const data = [
+    {
+        id: 1,
+        city: "Prague",
+        state: "Czech Republic",
+        airport: "Vaclav Havel Airport",
+        iata: "PRG",
+    },
+    {
+        id: 2,
+        city: "Los Angels",
+        state: "United States",
+        airport: "Los Angeles International Airport",
+        iata: "LAX",
+    },
+    {
+        id: 3,
+        city: "Bergstrom",
+        state: "Austria",
+        airport: "Austin-Bergstrom International Airport",
+        iata: "AUS",
+    },
+    {
+        id: 4,
+        city: "Beijing",
+        state: "China",
+        airport: "Beijing Capital International Airport",
+        iata: "PEK",
+    },
+    {
+        id: 4,
+        city: "New York",
+        state: "United States",
+        airport: "John F. Kennedy International Airport",
+        iata: "JFK",
+    },
+    {
+        id: 5,
+        city: "London",
+        state: "United Kingdom",
+        airport: "London Heathrow Airport",
+        iata: "LHR",
+    },
+    {
+        id: 6,
+        city: "London",
+        state: "United Kingdom",
+        airport: "LGW",
+        iata: "London Gatwick Airport",
+    },
+    {
+        id: 7,
+        city: "Rome",
+        state: "Italy",
+        airport: "Rome-Fiumicino",
+        iata: "FCO",
+    },
+    {
+        id: 8,
+        city: "Paris",
+        state: "France",
+        airport: "Charlese de Gaulla Airport",
+        iata: "CDG",
+    },
+    {
+        id: 9,
+        city: "Barcelona",
+        state: "Spain",
+        airport: "Barcelona-EL Prat Airport",
+        iata: "BCN",
+    },
+    {
+        id: 10,
+        city: "Viena",
+        state: "Austria",
+        airport: "Viena Airport",
+        iata: "VIE",
+    },
+    {
+        id: 11,
+        city: "Athens",
+        state: "Greece",
+        airport: "Charlese de Gaulla Airport",
+        iata: "CDG",
+    },
+    {
+        id: 12,
+        city: "Krakow",
+        state: "Poland",
+        airport: "John Paul II International Airport Kraków-Balice",
+        iata: "KRK",
+    }
+]
+
+export default data
--- Routes/FlightsRoute.js
+++ Routes/FlightsRoute.js
@@ -1,27 +1,19 @@
-import {getJson} from  'serpapi'
 import { configDotenv } from "dotenv";
+import data from "../Data/DummyData.js";
 
 configDotenv()
 async function FlightsRoute (fastify, options) {
     fastify.get('/Flights', async (req, rep) => {
-        try {
-            let data = getJson({
-                api_key: process.env.SERP_API,
-                engine: "google",
-                q: "Coffee",
-                location: "Austin, Texas, United States",
-                google_domain: "google.com",
-                gl: "us",
-                hl: "en"
-              }, (json) => {
-                console.log(json);
-              });
-              return "Uspech"
-        } catch (error) {
-            
-        }
+        const {departure, arriver} = req.query
 
-        return {hello: 'world'}
+        const results = data.map(airport => ({
+            name: airport.city,
+            state: airport.state,
+            airport: airport.airport,
+            iata: airport.iata
+        })).filter(airport => airport.iata === departure)
+        
+        return results[0].name
     })
 }
 
